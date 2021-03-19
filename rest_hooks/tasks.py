86970beb3a437cff042a0de8691fe2ad0abf26f5
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2021-03-19 18:12:07 -0400

    Add support for Celery 5

--- rest_hooks/tasks.py
+++ rest_hooks/tasks.py
@@ -1,35 +1,36 @@
 import requests
 import json
 
-from celery.task import Task
+from celery import shared_task
 
 from django.core.serializers.json import DjangoJSONEncoder
 
 from rest_hooks.utils import get_hook_model
 
 
-class DeliverHook(Task):
-    def run(self, target, payload, instance=None, hook_id=None, **kwargs):
-        """
-        target:     the url to receive the payload.
-        payload:    a python primitive data structure
-        instance:   a possibly null "trigger" instance
-        hook:       the defining Hook object (useful for removing)
-        """
-        response = requests.post(
-            url=target,
-            data=json.dumps(payload, cls=DjangoJSONEncoder),
-            headers={'Content-Type': 'application/json'}
-        )
+@shared_task
+def deliver_hook(target, payload, instance=None, hook_id=None, **kwargs):
+    """
+    target:     the url to receive the payload.
+    payload:    a python primitive data structure
+    instance:   a possibly null "trigger" instance
+    hook:       the defining Hook object (useful for removing)
+    """
+    response = requests.post(
+        url=target,
+        data=json.dumps(payload, cls=DjangoJSONEncoder),
+        headers={"Content-Type": "application/json"},
+    )
 
-        if response.status_code == 410 and hook_id:
-            HookModel = get_hook_model()
-            hook = HookModel.object.get(id=hook_id)
-            hook.delete()
+    if response.status_code == 410 and hook_id:
+        HookModel = get_hook_model()
+        hook = HookModel.object.get(id=hook_id)
+        hook.delete()
 
-        # would be nice to log this, at least for a little while...
+    # would be nice to log this, at least for a little while...
+
 
 def deliver_hook_wrapper(target, payload, instance=None, hook=None, **kwargs):
     if hook:
-        kwargs['hook_id'] = hook.id
-    return DeliverHook.delay(target, payload, **kwargs)
+        kwargs["hook_id"] = hook.id
+    return deliver_hook.delay(target, payload, **kwargs)
